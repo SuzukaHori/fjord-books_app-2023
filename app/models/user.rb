173dc575c4e353f6cf966dc99bc1076948c8031e
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-09 15:29:52 +0900

    画像のサイズ・見た目の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :avatar
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :icon, resize_to_fill: [100, 100]
+  end
 end

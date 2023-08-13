48831c6a1d0334e3678450e71226fefbc13cece0
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-13 21:31:24 +0900

    reportとuserの関連づけ
    reportテーブルにuser_idカラムを追加しました。

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -43,6 +43,6 @@ class ReportsController < ApplicationController
   end
 
   def report_params
-    params.require(:report).permit(:title, :content)
+    params.require(:report).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -49,10 +49,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_120936) do
   end
 
   create_table "reports", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,4 +75,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_120936) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reports", "users"
 end

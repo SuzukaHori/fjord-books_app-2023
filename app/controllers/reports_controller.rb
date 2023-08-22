3aadb1b57e4959e43d6d9d9bda36f80c001eceaf
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-22 16:37:59 +0900

    投稿日時の表示を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < ApplicationController
 
   # GET /reports or /reports.json
   def index
-    @reports = Report.all
+    @reports = Report.all.order(:id).page(params[:page])
   end
 
   def show

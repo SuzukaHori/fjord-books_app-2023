dd081ab9d0b5b52cf9b5251470be9f236e35c651
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-31 22:36:34 +0900

    ユーザ名の表示を共通化
    名前またはメールアドレスを表示する処理を使いまわせるよう、ヘルパーメソッドを定義しました。

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
   def current_user_name(user)
     [user.name, user.email].find(&:present?)
   end
+
+  def display_username_or_email(model)
+    if model.user.name.present? 
+      model.user.name 
+    else
+      model.user.email
+    end
+  end
 end

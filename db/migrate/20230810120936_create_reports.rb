6a978cb2349ce38a97482a8537b1e5dff84d7be9
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-08-31 10:19:35 +0900

    マイグレーションファイルにNOT NULL制約を追加

--- db/migrate/20230810120936_create_reports.rb
+++ db/migrate/20230810120936_create_reports.rb
@@ -1,8 +1,8 @@
 class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
-      t.belongs_to :user, foreign_key: true
-      t.string :title
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :title,    null: false
       t.text :content
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_014024) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_014024) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_014024) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.text "content"
+    t.text "content", null: false
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
     t.datetime "created_at", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_014024) do
 
   create_table "reports", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

d9f6f6593b02f74fad98dfe23ed30e72474a3b40
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-09-03 12:09:37 +0900

    nameまたはemailを表示する処理の書き方を変更
    ヘルパーでなくモデルに書くよう変更しました。

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,4 @@ module UsersHelper
   def current_user_name(user)
     [user.name, user.email].find(&:present?)
   end
-
-  def display_username_or_email(model)
-    (model.user.name.presence || model.user.email)
-  end
 end

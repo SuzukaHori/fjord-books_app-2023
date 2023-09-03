2329c440d54b9dd75587d307c7d918629f6acedd
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-09-03 14:32:55 +0900

    マイグレーションファイルを修正
    インデントやエラーが起きる箇所の修正を行いました。

--- db/migrate/20230903013630_remove_user_from_books.rb
+++ db/migrate/20230903013630_remove_user_from_books.rb
@@ -1,6 +1,5 @@
 class RemoveUserFromBooks < ActiveRecord::Migration[7.0]
   def change
-    remove_foreign_key :books, :user
     remove_reference :books, :user, null: false, foreign_key: true
   end
 end

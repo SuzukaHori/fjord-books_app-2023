dde0bf7becaae47376d6f69c00d6eaf81dba9520
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-09-19 15:12:45 +0900

    メンションのテストコードのリファクタリング

--- test/models/report_mention_test.rb
+++ test/models/report_mention_test.rb
@@ -5,11 +5,14 @@ require 'test_helper'
 class ReportMentionTest < ActiveSupport::TestCase
   test 'save_mentions' do
     user = FactoryBot.create(:suzuka)
+    FactoryBot.create(:first_report, user:)
 
-    report = user.reports.create!(title: 'メンションなしの日報', content: '今日も楽しく勉強した')
-    assert_equal [], report.mentioning_reports.ids
+    assert_no_difference 'ReportMention.count' do
+      user.reports.create!(title: 'メンションなしの日報', content: '今日も楽しく勉強した')
+    end
 
-    report = user.reports.create!(title: '1にメンションした日報', content: 'http://localhost:3000/reports/1')
-    assert_equal [1], report.mentioning_reports.ids
+    assert_difference 'ReportMention.count', 1 do
+      user.reports.create!(title: '1にメンションした日報', content: 'http://localhost:3000/reports/1')
+    end
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,4 +11,12 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'test_name'
     assert_equal 'test_name', user.name_or_email
   end
+
+  test 'current_user_name' do
+    user = FactoryBot.build(:suzuka)
+    assert_equal 'suzuka', current_user_name(user)
+
+    user = FactoryBot.build(:no_name)
+    assert_equal 'no_name@example.com', current_user_name(user)
+  end
 end

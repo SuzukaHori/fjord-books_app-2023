1ba58df0a87b144c1509c5a51071a10864b0fe10
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-09-25 16:51:05 +0900

    ミスの修正
    誤って変更したため、元に戻しました。

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -5,7 +5,7 @@ require 'application_system_test_case'
 class ReportsTest < ApplicationSystemTestCase
   setup do
     user = sign_in_with_email_and_password
-    @report = FactoryBot.create(user:)
+    @report = FactoryBot.create(:first_report, user:)
   end
 
   test 'visiting the index' do
